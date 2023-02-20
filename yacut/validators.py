50b3df8f176e4cd659957cc2a491859b88a1f940
alex-s-nik <to_ctrl@mail.ru>
2023-02-20 10:40:50 +0300

    fix: logic error

--- yacut/validators.py
+++ yacut/validators.py
@@ -2,7 +2,7 @@ from .utils import LOWER_LETTERS_N_DIGITS
 
 
 def is_not_letters_and_digits(s: str) -> bool:
-    return all(char in LOWER_LETTERS_N_DIGITS for char in s)
+    return not all(char in LOWER_LETTERS_N_DIGITS for char in s)
 
 
 def is_len_greater(s: str, max_length: int):
